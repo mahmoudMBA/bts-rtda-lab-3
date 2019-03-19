95f6f15d607752314cce13699318f3b6c1f02b38
Liesner <liesner@gmail.com>
2019-03-19 07:17:45 +0100

    adding O(lgn) and On2 alg

--- src/main/scala/Session1Snippets.scala
+++ src/main/scala/Session1Snippets.scala
@@ -1,16 +1,42 @@
 object Session1Snippets {
+
+  def _binarySearch(elements: Array[Int], elem: Int, from: Int, to: Int): Int = {
+    if(from > to)
+      return -1
+    val mid = (from + to) / 2
+    if(elements(mid) == elem)
+      return mid
+    if(elements(mid) > elem)
+      return _binarySearch(elements, elem, from, mid - 1)
+    return _binarySearch(elements, elem, mid + 1, to)
+  }
+
+  def binarySearch(elements: Array[Int], elem: Int) :Int = {
+    return _binarySearch(elements, elem, 0, elements.length - 1)
+  }
+
+
+  def multiply(matrix: Array[Array[Int]], vector: Array[Int]): Array[Int] = {
+    val resultVector:Array[Int] = Array.ofDim(matrix.length)
+    for(i <- 0 to matrix.length - 1) {
+      var dotProduct = 0;
+      for (j <- 0 to vector.length - 1){
+        dotProduct = dotProduct + matrix(i)(j)*vector(j)
+      }
+      resultVector(i) = dotProduct
+    }
+    return resultVector
+  }
+
   //code here
   def head(elements: Array[String]): String = {
     elements(0)
   }
 
-  def reverse(elements: Array[String]): Array[String] = {
-    val result = new Array[String](elements.length)
-
-    for(index <- elements.length - 1 to 0 by -1)
-      result(elements.length - (index + 1)) = elements(index)
-
-    result
+  def search(elements: Array[Int], elem: Int): Int = {
+    for(index <- 0 to elements.length - 1)
+      if(elements(index) == elem) return index
+    -1
   }
 
 }
--- src/test/scala/Session1SnippetsTest.scala
+++ src/test/scala/Session1SnippetsTest.scala
@@ -2,20 +2,59 @@ import org.scalatest.FunSuite
 
 class Session1SnippetsTest extends FunSuite {
 
-  test("testing head snippet") {
+  test("Head of array [\"e1\", \"e2\", \"e3\", \"e4\"] should be \"e1\"") {
     val elements:Array[String] = Array("e1", "e2", "e3", "e4")
 
-    assert(Session1Snippets.head(elements)==="e1")
+    assert(Session1Snippets.head(elements) === "e1")
   }
 
-  test("Testing reverse array snippet") {
-    val elements:Array[String] = Array("e1", "e2", "e3", "e4")
-    val reverseElementsExpected:Array[String] = Array("e4", "e3", "e2", "e1")
-    val reverseElements = Session1Snippets.reverse(elements)
-    assert(reverseElements.deep == reverseElementsExpected.deep)
+  test("Linear search the elem 5 in array [6, 1, 3, 0, 5, 6] should return index 4") {
+    val elements:Array[Int] = Array(6, 1, 3, 0, 5, 6)
+    val indexElem = Session1Snippets.search(elements, 5)
+    assert(indexElem === 4)
   }
-  
-  test("Testing ")
+
+
+  test("Linear search the elem 7 in array [6, 1, 3, 0, 5, 6] should return index -1") {
+    val elements:Array[Int] = Array(6, 1, 3, 0, 5, 6)
+    val indexElem = Session1Snippets.search(elements, 7)
+    assert(indexElem === -1)
+  }
+
+  test("Multiplying identity matrix by a vector should return the same vector"){
+    val matrix =  Array(Array(1, 0, 0), Array(0, 1, 0), Array(0, 0, 1))
+    val vector = Array(2, 3, 1)
+    val resultVector = Session1Snippets.multiply(matrix, vector)
+    assert(resultVector.length === matrix.length)
+    assert(resultVector.deep === vector.deep)
+  }
+
+  test("Multiplying all 1 (4x3) matrix by a vector should return " +
+                "a vector (3) with all elements equal to the sum"){
+    val matrix =  Array(Array(1, 1, 1, 1), Array(1, 1, 1, 1), Array(1, 1, 1, 1))
+    val vector = Array(1, 3, 2, 1)
+    val resultVector = Session1Snippets.multiply(matrix, vector)
+    assert(resultVector.length === matrix.length)
+    assert(resultVector.deep === Array(7,7,7).deep)
+  }
+
+  test("Binary search the elem 12 in array [3, 4, 6, 8, 12, 14] should return index 4"){
+    val elements:Array[Int] = Array(3, 4, 6, 8, 12, 14)
+    val indexElem = Session1Snippets.binarySearch(elements, 12)
+    assert(indexElem === 4)
+  }
+
+  test("Binary search the elem 1 in array [3, 4, 6, 8, 12, 14] should return index -1"){
+    val elements:Array[Int] = Array(3, 4, 6, 8, 12, 14)
+    val indexElem = Session1Snippets.binarySearch(elements, 1)
+    assert(indexElem === -1)
+  }
+
+  test("Binary search the elem 8 in array [3, 4, 6, 8, 12, 14] should return index 3"){
+    val elements:Array[Int] = Array(3, 4, 6, 8, 12, 14)
+    val indexElem = Session1Snippets.binarySearch(elements, 8)
+    assert(indexElem === 3)
+  }
 
 
 }
