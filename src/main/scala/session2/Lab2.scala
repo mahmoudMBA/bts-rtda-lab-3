34279b0157fb45510285e8c081cb6a1fc4357dba
Liesner <liesner@gmail.com>
2019-04-12 07:53:16 +0200

    init commit

---
+++ src/main/scala/session2/Lab2.scala
@@ -0,0 +1,11 @@
+package session2
+
+object Lab2 {
+  def min(array: Array[Int]) = ???
+
+  def max(array: Array[Int]) = ???
+
+  def avg(array: Array[Double]): Double = ???
+
+  def minMaxDiff(array: Array[Int]) = ???
+}
---
+++ src/test/scala/session2/Lab2Test.scala
@@ -0,0 +1,55 @@
+package session2
+
+import org.scalatest.FunSuite
+
+class Lab2Test extends FunSuite {
+  /*
+    Given and array of elements calculate the sum
+  */
+  test("The sum of array [1, 3, 5, 6] should be 15"){
+    val expectedSum = 15
+    val array: Array[Int] = Array(1,3,5,6)
+    val realSum: Integer = Lab2.max(array)
+    assert(expectedSum == realSum)
+  }
+
+  /*
+    Given and array of elements calculate the max
+  */
+  test("The max of array [1, 3, 5, 6] should be 6"){
+    val expectedMax = 6
+    val array: Array[Int] = Array(1,3,5,6)
+    val realMax: Integer = Lab2.max(array)
+    assert(expectedMax == realMax)
+  }
+
+  /*
+  Given and array of elements calculate the min
+*/
+  test("The min of array [1, 3, 5, 6] should be 5"){
+    val expectedMin = 1
+    val array: Array[Int] = Array(1,3,5,6)
+    val realMin: Integer = Lab2.min(array)
+    assert(expectedMin == realMin)
+  }
+
+  /*
+    Given and array of elements calculate the diff between max and min
+  */
+  test("The between max and min of array [1, 3, 5, 6] should be 5"){
+    val expectedDiff = 5
+    val array: Array[Int] = Array(1,3,5,6)
+    val realDiff: Integer = Lab2.minMaxDiff(array)
+    assert(expectedDiff == realDiff)
+  }
+
+  /*
+    Given and array of "Double" elements calculate the average
+  */
+  test("The avg of array [1.0,1.0,1.0,1.0] should be approximately 1.0"){
+    val expectedAvg: Double = 1.0
+    val array: Array[Double] = Array(1.0,1.0,1.0,1.0)
+    val realAvg: Double = Lab2.avg(array)
+    assert(Math.abs(expectedAvg - realAvg) < 0.00000001)
+  }
+}
